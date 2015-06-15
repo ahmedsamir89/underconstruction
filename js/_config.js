83023ea9cfac87a3a6d5e39346cbd97cbcb609fd
Ahmed Samir <ahmed.samir@camelcasetech.com>
2015-06-15 12:54:17 +0200

    final ISA

--- js/_config.js
+++ js/_config.js
@@ -2,6 +2,6 @@
 
 var percent_ready = 72, // Enter here value of readiness your website
 
-	SiteStartDate = '2015/07/14 07:30', // The Date Of Site Opening
+	SiteStartDate = '2016/01/14 07:30', // The Date Of Site Opening
 	
 	YoutubeBgCode = '8oKZkEsZd6c'; // The Part Of Youtube Video URL
